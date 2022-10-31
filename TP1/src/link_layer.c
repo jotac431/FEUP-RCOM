2439eae3dfda170690168f66bba9b95e97f05350
jotac431 <jotac431@gmail.com>
2022-10-31 19:08:01 +0000

    d

--- TP1/src/link_layer.c
+++ TP1/src/link_layer.c
@@ -370,7 +370,7 @@ int llwrite(const unsigned char *buf, int bufSize)
             bytes = write(fd, msg, newSize);
             printf("Data Enviada. %d bytes written\n", bytes);
             numtries++;
-            alarm(layer.timeout); // Set alarm to be triggered
+            alarm(layer.timeout); // Set alarm to be triggeredd
             alarmEnabled = TRUE;
             state = START;
         }
